52013ebd86716ccd3e592307ddb34be6b8736efc
Jun Hayashi <kevin.jun.hayashi@gmail.com>
2023-11-25 10:26:43 -0300

    api test

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('search-images')
+  async searchImages(@Query('query') query: string) {
+    return this.appService.searchImages(query);
+  }
+
+  @Get('asset-manifest')
+  async getAssetManifest(@Query('nasaId') nasaId: string) {
+    return this.appService.getAssetManifest(nasaId);
+  }
+
+  @Get('asset-metadata')
+  async getAssetMetadata(@Query('nasaId') nasaId: string) {
+    return this.appService.getAssetMetadata(nasaId);
+  }
+
+  @Get('video-captions')
+  async getVideoCaptions(@Query('nasaId') nasaId: string) {
+    return this.appService.getVideoCaptions(nasaId);
   }
 }
