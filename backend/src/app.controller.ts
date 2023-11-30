eea17723a799ee1e2e0d53c6b577758825613997
Jun Hayashi <kevin.jun.hayashi@gmail.com>
2023-11-29 21:05:32 -0300

    SNS Share

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -23,5 +24,35 @@ export class AppController {
   @Get('video-captions')
   async getVideoCaptions(@Query('nasaId') nasaId: string) {
     return this.appService.getVideoCaptions(nasaId);
+  }
+
+  @Get('share-instagram')
+  async shareInstagram(@Query('nasaId') nasaId: string, @Query('caption') caption: string, @Res() res: Response) {
+    const instagramUrl = this.appService.getInstagramShareUrl(nasaId, caption);
+    return res.redirect(instagramUrl);
+  }
+  
+  @Get('share-twitter')
+  async shareTwitter(@Query('nasaId') nasaId: string, @Query('text') text: string, @Res() res: Response) {
+    const twitterUrl = this.appService.getTwitterShareUrl(nasaId, text);
+    return res.redirect(twitterUrl);
+  }
+
+  @Get('share-facebook')
+  async shareFacebook(@Query('nasaId') nasaId: string, @Query('caption') caption: string, @Res() res: Response) {
+    const facebookUrl = this.appService.getFacebookShareUrl(nasaId, caption);
+    return res.redirect(facebookUrl);
+  }
+
+  @Get('share-whatsapp')
+  async shareWhatsApp(@Query('nasaId') nasaId: string, @Query('text') text: string, @Res() res: Response) {
+    const whatsappUrl = this.appService.getWhatsAppShareUrl(nasaId, text);
+    return res.redirect(whatsappUrl);
+  }
+
+  @Get('share-telegram')
+  async shareTelegram(@Query('nasaId') nasaId: string, @Query('text') text: string, @Res() res: Response) {
+    const telegramUrl = this.appService.getTelegramShareUrl(nasaId, text);
+    return res.redirect(telegramUrl);
   }
 }
